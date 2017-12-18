6cc3030ba1da58b129d4d9744df379801f72e769
yyy <18311161659@163.com>
2017-12-19 07:55:14 +0800

    jiekou

--- app/Http/Controllers/HomeloginController.php
+++ app/Http/Controllers/HomeloginController.php
@@ -1,13 +1,13 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class HomeloginController extends Controller{
 
     //注册接口
     public function reg(Request $request){
         $user = $request->input();
-        var_dump($user);
+        echo "<pre>";
+        print_r($user);
     }
 }
